13a5ca226e4ce0524c7a2fa349b39b35393c6073
RicardoSchueler <vinip.schueler@gmail.com>
2025-01-11 12:07:25 -0300

    Funcionalidade Salvar com mensagem
    Inclusão da funcionalidade Salvar para que os itens sejam cadastrados no banco de dados, com mensagem informando se esse cadastro foi realizado com sucesso ou não.

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,42 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 
 import java.sql.PreparedStatement;
-import java.sql.Connection;
-import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.sql.SQLException;
 
 
-public class ProdutosDAO {
-    
-    Connection conn;
+public class ProdutosDAO 
+{        
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+    boolean acessoDB; 
+    int status;
     
-    public void cadastrarProduto (ProdutosDTO produto){
+    public int cadastrarProduto (ProdutosDTO produto)
+    {                
+        conectaDAO conector = new conectaDAO(); 
+        acessoDB = conector.connectDB();
+        //acessoDB = new conectaDAO().connectDB();
         
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        if (acessoDB == false)
+        {
+            //JOptionPane.showMessageDialog(null,"Erro de conexão");
+            return 0;
+        }
+        else
+        {
+            try 
+            {                
+                prep = conector.conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES(?,?,?)");                
+                prep.setString(1,produto.getNome());
+                prep.setInt(2,produto.getValor());
+                prep.setString(3, produto.getStatus());
+                status = prep.executeUpdate();
+                return status; //retornar 1
+            } 
+            catch (SQLException ex) 
+            {
+                System.out.println("Erro ao gravar: " + ex.getMessage());
+                return ex.getErrorCode();
+            } 
+        }        
     }
     
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+    
+    public ArrayList<ProdutosDTO> listarProdutos()
+    {        
         return listagem;
-    }
-    
-    
-    
+    }       
         
 }
 
--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -4,30 +4,50 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-public class conectaDAO {
+        
+        
+public class conectaDAO 
+{
     
-    public Connection connectDB(){
-        Connection conn = null;
+    Connection conn = null;   //criando um objeto do tipo connection chamado conn
+
+    public String url = "jdbc:mysql://localhost:3306/senac_ead"; //Nome da base de dados
+    public String user = "root"; //nome do usuário do MySQL
+    public String password = "r32Nit06$"; //senha do MySQL 
         
-        try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
-        }
-        return conn;
+    public boolean connectDB()
+    {        
+        try 
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, user, password);
+            // System.out.println("Conexão realizada com sucesso");
+                        
+            return true;
+        } 
+        catch (ClassNotFoundException | SQLException ex) 
+        {
+            //JOptionPane.showMessageDialog(null, "Falha na conexão com o banco " + ex.getMessage());
+            return false;
+        }       
+        // return conn;
     }
+    
+        
+    public void disconnectDB()
+    {
+        try 
+        {
+            if (conn != null && !conn.isClosed()) 
+            {
+                conn.close();
+                // System.out.println("Voce se desconectou do banco de dados.");
+            }
+        }        
+        catch (SQLException ex) 
+        {
+             System.out.println("Nao foi possivel desconectar do banco dados.");
+        }
+    }    
     
 }
